b4afaa5351fbd598c9be7455ba3dad9da62c343e
Simon Søllingvraa <sso@danskecommodities.com>
2016-05-05 23:05:19 +0200

    Added simple accordion component

---
+++ public/app/accordion.component.js
@@ -0,0 +1,67 @@
+(function () {
+    'use strict';
+
+    var module = angular.module('squares');
+
+    module.component('accordion', {
+        transclude: true,
+        templateUrl: '/public/app/accordion.component.html',
+        controllerAs: 'model',
+        controller: function () {
+            var model = this;
+            var panels = [];
+
+            model.selectPanel = function (panel) {
+                for (var i = 0; i < panels.length; i++) {
+                    if (panel === panels[i]) {
+                        panels[i].turnOn();
+                    }
+                    else {
+                        panels[i].turnOff();
+                    }
+                }
+            }
+
+            model.addPanel = function (panel) {
+                panels.push(panel);
+
+                if (panels.length > 0) {
+                    panels[0].turnOn();
+                }
+            }
+
+        }
+    });
+
+    module.component('accordionPanel', {
+        bindings: {
+            heading: '@'
+        },
+        require: {
+            parent: '^accordion'
+        },
+        controllerAs: 'model',
+        controller: function () {
+            var model = this;
+            model.selected = false;
+
+            model.$onInit = function() {
+                model.parent.addPanel(model)
+            };
+            
+            model.turnOn = function() {
+                model.selected = true;
+            };
+            
+            model.turnOff = function() {
+                model.selected = false;
+            };
+            
+            model.select = function() {
+                model.parent.selectPanel(model);
+            };
+        },
+        transclude: true,
+        templateUrl: '/public/app/accordion-panel.component.html'
+    });
+})();
