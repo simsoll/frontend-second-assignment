ccabd5e8d86d0b6963170f0c27de24c30076f858
Simon Søllingvraa <sso@danskecommodities.com>
2016-05-08 16:26:29 +0200

    Added login page

---
+++ public/app/pages/login/login.component.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+    
+    var module = angular.module('squares');
+    
+    var controller = function() {
+        
+    };
+    
+    module.component('login', {
+        templateUrl: '/public/app/pages/login/login.component.html',
+        controllerAs: 'model',
+        controller: controller
+    });
+})();
