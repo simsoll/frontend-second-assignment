e102bd7f7fa29ea6057b2bc284aae2317ed94781
simsoll <simsoll@users.noreply.github.com>
2016-05-16 20:13:54 +0200

    Moved rating part out of the review component and into its own component

--- public/app/components/review/review.component.js
+++ public/app/components/review/review.component.js
@@ -4,21 +4,6 @@
 
     var controller = function () {
         var model = this;
-
-        model.$onInit = function () {
-            model.entries = buildEntries(model.review.rating, model.max);
-        };
-
-        function buildEntries(value, max) {
-            var entries = [];
-
-            for (var i = 0; i < max; i++) {
-                var icon = i < value ? 'glyphicon-star' : 'glyphicon-star-empty';
-                entries.push(icon);
-            }
-
-            return entries;
-        }
     }
 
     module.component('review', {
