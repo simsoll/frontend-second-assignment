a9ee25199bc496f44831e0c50c1ead190353ddab
simsoll <simsoll@users.noreply.github.com>
2016-05-16 20:34:56 +0200

    Added average ratings to square sets and arts

--- public/app/components/art/art-piece.component.js
+++ public/app/components/art/art-piece.component.js
@@ -3,8 +3,20 @@
 
     function controller() {
         var model = this;
+        model.averageRating = averageRating;
+
+        function averageRating() {
+            var sum = 0;
+            var count = model.art.reviews.length;
+
+            for (var i = 0; i < count; i++) {
+                sum += model.art.reviews[i].rating;
+            }
+
+            return count > 0 ? Math.ceil(sum / count) : 0;
+        }
     }
-    
+
     var module = angular.module('squares');
 
     module.component('artPiece', {
--- public/app/components/square-set/square-set.component.js
+++ public/app/components/square-set/square-set.component.js
@@ -3,6 +3,18 @@
 
     function controller() {
         var model = this;
+        model.averageRating = averageRating;
+
+        function averageRating() {
+            var sum = 0;
+            var count = model.squareSet.reviews.length;
+
+            for (var i = 0; i < count; i++) {
+                sum += model.squareSet.reviews[i].rating;
+            }
+
+            return count > 0 ? Math.ceil(sum / count) : 0;
+        }        
     }
     
     var module = angular.module('squares');
