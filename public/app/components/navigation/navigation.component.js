d36fac8e19779d16eb450772f9dc0effd63317af
simsoll <simsoll@users.noreply.github.com>
2016-05-18 22:34:42 +0200

    Added some navigation styling

--- public/app/components/navigation/navigation.component.js
+++ public/app/components/navigation/navigation.component.js
@@ -3,10 +3,18 @@
 
     var module = angular.module('squares');
 
-    var controller = function ($http) {
+    var controller = function ($http, $location) {
         var model = this;
-
+        model.isActive = isActive;
         model.logOut = logOut;
+
+        function isActive(route) {
+            if (route === '/') {
+                return $location.path() === '/'; 
+            }
+            
+            return $location.path().startsWith(route);
+        }
 
         function logOut() {
             $http.get('/api/user/logOut').then(function (result) {
