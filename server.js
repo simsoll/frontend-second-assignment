24ddc94deb4000dbce536822f0b09ca874b97368
simsoll <simsoll@users.noreply.github.com>
2016-05-21 13:12:48 +0200

    Improved uploading functionality and styling

--- server.js
+++ server.js
@@ -17,14 +17,16 @@ app.set('port', process.env.PORT || 5000);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add routes
 require(path.join(__dirname, 'routes.js'))(app);
 
-app.listen(app.get('port'), function(err) {
+app.listen(app.get('port'), function (err) {
     console.log('running server on port ' + app.get('port'));
 });
